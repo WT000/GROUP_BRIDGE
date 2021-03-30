785e971f48d2015168fab32ae5715c9ea14e18bf
hairicko21 <5deyyh32@solent.ac.uk>
2021-03-30 14:02:01 +0300

    car updated

--- conftest.py
+++ conftest.py
@@ -1,11 +1,9 @@
 import pytest
 from Motorbike import Motorbike
 from Lorry import Lorry
-<<<<<<< HEAD
 from Car import Car
-=======
 from Bridge import Bridge
->>>>>>> 17fd87328541c394427f298aba725b1a00341d0c
+
 
 
 @pytest.fixture
@@ -18,10 +16,10 @@ def lorry():
     yield Lorry("AB1 3IE", 3000)
 
 @pytest.fixture
-<<<<<<< HEAD
 def car():
     yield Car("A6423KM", 1234, "Car")
-=======
+
+@pytest.fixture
 def bridge():
     yield Bridge()
->>>>>>> 17fd87328541c394427f298aba725b1a00341d0c
+
