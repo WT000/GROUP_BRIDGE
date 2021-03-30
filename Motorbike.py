d6e5f32b229f6347fafcb9228795f69e486b28dc
ismailkefokeris <ismailkefokeris@gmail.com>
2021-03-30 11:21:10 +0100

    Creation of default motorbike class and typical tests

--- Motorbike.py
+++ Motorbike.py
@@ -4,7 +4,7 @@ from Vehicle import Vehicle
 class Motorbike(Vehicle):
 
     def __init__(self, vehicle_type, reg_num, weight):
-        pass
+        super().__init__(vehicle_type, reg_num, weight)
 
     def calculateFee(self):
         return 3.00
--- conftest.py
+++ conftest.py
@@ -1,6 +1,8 @@
 import pytest
-import Motorbike
+from Motorbike import Motorbike
+from Vehicle import Vehicle
 
 
+@pytest.fixture
 def default_motorbike():
     yield Motorbike("Motorbike", 5050, 150)
