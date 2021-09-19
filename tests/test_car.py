fd244df2550b4401fe243526ab98bdf159612f16
WT000 <55751558+WT000@users.noreply.github.com>
2021-09-19 19:34:30 +0100

    Update test_car.py
    Better tests, now it will test the car beforoe and after the overweight limit.

--- tests/test_car.py
+++ tests/test_car.py
@@ -7,9 +7,21 @@ class TestCar:
     def testCarExist(self, car):
         assert isinstance(car, Car)
 
-    def testCarNormalWeight(self,car):
+    def testCarNormalWeight(self, car):
         assert car.calculateFee() == float(5.00)
 
-    def testCarFat(self,car):
+    def testCarExactOverweight(self, car):
         car.weight = 1690
         assert car.calculateFee() == 5.1
+        
+    def testCarBeforeOverweight(self, car):
+        car.weight = 1689
+        assert car.calculateFee() == 5.0
+        
+    def testCarAfterOverweight(self, car):
+        car.weight = 1691
+        assert cart.calculateFee() == 5.1
+        
+    def testCarSecondOverweight(self, car):
+        car.weight = 1790
+        assert car.CalculateFee() == 5.2
